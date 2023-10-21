5f86726dc7bdcaec725d0ab1d33034d17935812b
Lasty <lasty@MacBook-Washington.local>
2023-10-21 08:05:07 +0300

    processing histories (not completed)

--- Model.py
+++ Model.py
@@ -1,4 +1,4 @@
-import chess #, keras
+import chess, re #, keras
 import pandas as pd
 import numpy as np
 import matplotlib as plt
@@ -17,11 +17,10 @@ else:
 
 coloumns = ['Event','White','Black','Result','UTCDate','UTCTime','WhiteElo','BlackElo','WhiteRatingDiff','BlackRatingDiff','ECO','Opening','TimeControl','Termination','AN']
 
-df = pd.read_csv(name, names= coloumns)[0:10]
+df = pd.read_csv(name, names= coloumns)[:10]
 
 print(df)
 # prepare massives
-print("Started...")
 
 combination_code = ''
 
@@ -39,12 +38,25 @@ def piece(board, cell):
         return str(board.piece_at(cell))
     else:
         return '.'
-    
-histories = [key for key in df.AN]
+
+def refactor_histories(history): # fix sub func for delete all counts
+    temp = ""
+    while temp != history:
+        temp = history
+        history = re.sub(r'^[0-9]*[.]', r'', history)
+
+    return history
+
+histories = [key[:-4] for key in df.AN]  #create re for true split !fix
+
+histories = [refactor_histories(history)[1:] for history in histories]
+histories = [re.sub(r'  ', r' ', history) for history in histories]
+
+
 
 status = [1 if key == '1-0' else 0 for key in df.Result]
 
-print('Started..')
+print('Started..') # flag for new block
 
 for histories_index, this_history in enumerate(histories):
     if histories_index % 1000 == 0: print(histories_index)
@@ -53,13 +65,13 @@ for histories_index, this_history in enumerate(histories):
     fen = board.fen()
     combination_code = [PIECE_SYMBOLS_NEW.index(piece(board, cell)) for cell in range(64)] 
 
-    for index, key in enumerate(histories[histories_index].split()[:10]): #create re for true split !fix
+    for index, key in enumerate(histories[histories_index].split()[:10]):
         print(key)
         board.push_san(key)
 
         if index % 2 == 0: 
             fen = board.fen()
-            combination_code = [PIECE_SYMBOLS_NEW.index(piece(cell)) for cell in range(64)]
+            combination_code = [PIECE_SYMBOLS_NEW.index(piece(board, cell)) for cell in range(64)]
 
 
             x.append(combination_code)

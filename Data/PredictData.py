2c15de82e1a551d5d6ac8af3be7b5421e43e1443
Lasty <lasty@MacBook-Washington.local>
2023-11-04 21:56:09 +0300

    Create prediction functional

--- Data/PredictData.py
+++ Data/PredictData.py
@@ -5,20 +5,30 @@ import numpy as np
 # colour for bot 1-white, 0-black
 def prepare_predict(board:chess.Board, colour:bool): #add support for black
     """colour for model - 1 if white else 0"""
-    bin_board = ModelData.convert_unicode(board.unicode())
     moves = []
     ins = []
     if colour:
+        bin_board = ModelData.convert_unicode(board.unicode())
         for move in board.legal_moves:
             moves.append(move.uci())
             board.push(move)
             ins.append(np.array([*bin_board, *ModelData.convert_unicode(board.unicode())]))
             board.pop()
+    else:
+        bin_board = ModelData.convert_unicode(board.mirror().unicode())
+        for move in board.legal_moves:
+            moves.append(move.uci())
+            board.push(move)
+            ins.append(np.array([*bin_board, *ModelData.convert_unicode(board.mirror().unicode())]))
+            board.pop()
+
+
     return (np.array(moves), np.array(ins))
 
             
 
 if __name__ == '__main__':
     from ModelData import ModelData
+
 else:
     from .ModelData import ModelData
--- Model.py
+++ Model.py
@@ -8,43 +8,46 @@ from keras.losses import binary_crossentropy
 from keras.saving import load_model
 import matplotlib.pyplot as plt
 
+import chess
+# import numpy as np
 
 class Model:
 
-    def create_model(self) -> Sequential:
-        model = Sequential([
+    def create_model(self) -> None:
+        self.model = Sequential([
             Dense(1024, activation='tanh', input_shape=(512,)),
             Dense(1, activation='sigmoid')
         ])
 
         myAdam = Adam(learning_rate=0.001)
-        model.compile(optimizer=myAdam,
+        self.model.compile(optimizer=myAdam,
                     loss=binary_crossentropy,
                     metrics=['accuracy'])
-        return model
+        
     
-    def save_model(self, model) -> None:
-        model.save("saved_model/")
+    def save_model(self) -> None:
+        self.model.save("saved_model/")
 
-    def load_model(self):
-        model = load_model("saved_model/")
-        return model
+    def load_model(self) -> None:
+        self.model = load_model("saved_model/")
 
-    def main(self, x_train, y_train):
+    def fit(self, x, y, batch_size=128, epochs=100, validation_split=0.2):
+        self.history = self.model.fit(x=x,y=y, batch_size=batch_size,
+                                    epochs=epochs, verbose=1,
+                                    validation_split=validation_split).history
+    def predict(self, board, colour):
+        """board:chess.Board, colour:Bool (1-white, 0, black)"""
+        moves, x = prepare_predict(board, colour)
+        predict = self.model.predict(x, verbose=0)
+        return moves[predict.argmax()]
+        
 
-        # model = self.load_model()
-        model = self.create_model()
+        
 
-        history = model.fit(x_train, y_train, batch_size=128, epochs=100, validation_split=0.2)
-
-        self.save_model(model)
-
-        self.create_plot(history, "loss")
-        self.create_plot(history, "accuracy")
-    
-    def create_plot(self, history, metrics:str) -> None:
-        plt.plot(history.history[metrics])
-        plt.plot(history.history['val_' + metrics])
+    def create_plot(self, metrics:str) -> None:
+        plt.plot(self.history[metrics])
+        print(type(self.history))
+        plt.plot(self.history['val_' + metrics])
         plt.title("model " + metrics)
         plt.ylabel(metrics)
         plt.xlabel("epoch")
@@ -54,10 +57,23 @@ class Model:
 
 if __name__ == '__main__':
     from Data.ModelData import ModelData
+    from Data.PredictData import prepare_predict
 
-    x, y = ModelData(200).create_inputs()
+    # x, y = ModelData(200).create_inputs()
     model = Model()
-    model.main(x, y)
+
+    model.load_model()
+    # model.create_model()
+    board = chess.Board()
+    board.push_san("e2e4")
+    print(model.predict(board, 0))
+
+    # model.fit(x, y, batch_size=128, epochs=10, validation_split=0.2)
+    # model.create_plot("loss")
+    # model.create_plot("accuracy")
+
+
 else:
     from .Data.ModelData import ModelData
+    from .Data.PredictData import prepare_predict
 
